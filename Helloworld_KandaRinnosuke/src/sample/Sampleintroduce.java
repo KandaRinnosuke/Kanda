94a351b5a8fc9c21a1d0ca574a7d90bbc85b7bd4
kr315 <kr315@192.168.2.116>
2025-06-12 20:46:06 +0900

    課題7.3提出

---
+++ Helloworld_KandaRinnosuke/src/sample/Sampleintroduce.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class Sampleintroduce {
+
+	public static void main(String[] args) {
+		//名前
+		String name ="神田　麟之介";
+		//年齢
+		int age= 19;
+		//趣味
+		String hobby ="カラオケ";
+		
+		
+	    /*
+	     * 自己紹介文の出力処理
+	     */
+		
+		System.out.println("私の名前は"+name+"です。");
+		System.out.println("年齢は"+age+"です。");
+		System.out.println("趣味は"+hobby+"です。");
+
+	}
+
+}
